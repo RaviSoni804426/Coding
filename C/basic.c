1cd5bc9729227555fbe49cb7244e466fa97d4334
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-06-24 11:16:27 +0530

    first commit

---
+++ C/basic.c
@@ -0,0 +1,25 @@
+// #include<stdio.h>
+// int main(){
+//    int number;
+//    scanf("%d",&number);
+//     printf("%d",number);
+//     printf("\nthis is my first code\n");
+//     return 0;
+
+// }
+//this is my first code
+/*this is teachen by sharda mam*/
+// 
+// #include<stdio.h>
+// int main(){
+//     float radius;
+//     printf("Enter radius  of circle: ");
+//     scanf("%f",&radius);
+//     printf("Area of the circle is: %f",3.14*radius*radius);
+//     return 0;
+#include<stdio.h>
+#include<math.h>
+int main({
+    int b,c;
+    int power=
+})
