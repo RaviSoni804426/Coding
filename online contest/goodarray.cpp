bd5f5b6b424b84da233f3a98ad0de7edacf3753e
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-06-27 13:47:32 +0530

    online contest

---
+++ online contest/goodarray.cpp
@@ -0,0 +1,34 @@
+int main()
+{
+    ll m,n,i,j=0,res,x,y,p;
+
+    cin>>n;
+    ll a[n];
+
+    ll sum=0, ans=0;
+
+    fr(i, n)cin>>a[i] , ans+=a[i] , cnt[a[i] ]++;
+
+    vector<ll>v;
+    fr(i, n)
+    {
+        x=0;
+        ans-=a[i];
+        ll tmp=ans/2;
+        if(cnt[a[i]]) x=1, cnt[a[i] ]--;
+
+       if(ans%2==0 and  cnt[tmp] )v.push_back(i+1);
+
+        ans+=a[i];
+        if(x) cnt[a[i] ]++;
+    }
+
+    if(v.size())
+    {
+        cout<<v.size()<<endl;
+        fr(i, v.size())cout<<v[i]<<" ";
+    }
+    else cout<<0<<endl;
+
+
+}
